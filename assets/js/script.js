82a192f141d165cf7cfbf18797ed010d4514d213
stephen-scheiman <sbscheiman@gmail.com>
2023-12-05 08:09:48 -0800

    Updates to weather api params

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,59 @@
+//All the action happens when the Search button is clicked
 $("#searchButton").on("click", function () {
+  // Get the city name from the user
   var cityName = $('input[name="locationField"]').val();
-
+  //Make sure it isn't blank
+  if (!cityName) {
+    return;
+  }
+  // This variable will hold the results of the city name to lat/lon conversion
   var geoCodeURL =
     "https://api.openweathermap.org/geo/1.0/direct?q=" +
     cityName +
-    "&limit=1&appid=46829ce23a8c173b9cc4d4f2145d78e5";
-
+    "&limit=5&appid=46829ce23a8c173b9cc4d4f2145d78e5";
+//Get the data from the api
   fetch(geoCodeURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
+      //store the returned lat/long
       var latitude = data[0].lat;
       var longitude = data[0].lon;
-      console.log("Latitude: " + latitude);
-      console.log("Longitude: " + longitude);
-      $("#priorCities").append('<li>'+ cityName +'</li>');
-      var forecastURL =
-        "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+//add the city to the prior search list and local storage
+      $("#priorCities").append("<li>" + cityName + "</li>");
+      localStorage.setItem("city", cityName);
+
+      $('input[name="locationField"]').val("");
+//This variable will hold the current conditions for the searched city
+      var currentConditionsURL =
+        "https://api.openweathermap.org/data/2.5/weather?lat=" +
         latitude +
         "&lon=" +
         longitude +
-        "&appid=46829ce23a8c173b9cc4d4f2145d78e5";
-      fetch(forecastURL)
+        "&units=imperial&appid=46829ce23a8c173b9cc4d4f2145d78e5";
+      fetch(currentConditionsURL)
         .then(function (response) {
           return response.json();
         })
         .then(function (data) {
           console.log(data);
+         var forecastTime = data.dt;
+         console.log(dayjs(forecastTime, 'HH:mm'));
         });
     });
+
+  // var forecastURL =
+  //   'https://api.openweathermap.org/data/2.5/forecast?lat=' +
+  //   latitude +
+  //   '&lon=' +
+  //   longitude +
+  //   '&appid=46829ce23a8c173b9cc4d4f2145d78e5';
+  // fetch(forecastURL)
+  //   .then(function (response) {
+  //     return response.json();
+  //   })
+  //   .then(function (data) {
+  //     console.log(data);
+  //   });
 });
