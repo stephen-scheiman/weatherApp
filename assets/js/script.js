166a2c020d6b09bcfb24952b3e3027672dfb9408
stephen-scheiman <sbscheiman@gmail.com>
2023-12-05 09:15:15 -0800

    Working on forecast display

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ $("#searchButton").on("click", function () {
     "https://api.openweathermap.org/geo/1.0/direct?q=" +
     cityName +
     "&limit=5&appid=46829ce23a8c173b9cc4d4f2145d78e5";
-//Get the data from the api
+  //Get the data from the api
   fetch(geoCodeURL)
     .then(function (response) {
       return response.json();
@@ -20,12 +20,12 @@ $("#searchButton").on("click", function () {
       //store the returned lat/long
       var latitude = data[0].lat;
       var longitude = data[0].lon;
-//add the city to the prior search list and local storage
+      //add the city to the prior search list and local storage
       $("#priorCities").append("<li>" + cityName + "</li>");
       localStorage.setItem("city", cityName);
 
       $('input[name="locationField"]').val("");
-//This variable will hold the current conditions for the searched city
+      //This variable will hold the current conditions for the searched city
       var currentConditionsURL =
         "https://api.openweathermap.org/data/2.5/weather?lat=" +
         latitude +
@@ -37,9 +37,12 @@ $("#searchButton").on("click", function () {
           return response.json();
         })
         .then(function (data) {
-          console.log(data);
-         var forecastTime = data.dt;
-         console.log(dayjs(forecastTime, 'HH:mm'));
+          // var forecastTime = dayjs();
+          // var fcastText = document.getElementById("forecastText");
+          // fcastText.innerHTML = dayjs(forecastTime).format("MM/DD/YYYY hh:mm a");
+          var forecastText = data.main;
+          var fcastTextEl = document.getElementById("forecastText");
+          fcastTextEl.innerHTML = forecastText;
         });
     });
 
